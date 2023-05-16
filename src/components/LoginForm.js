d70e22b3d60bf728202e07fc7994598cfc3d8e75
Julia Guiomar Niso Galán <guiomar.niso@ctb.upm.es>
2023-05-16 18:46:10 +0200

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,25 @@
+//GH
+//AUTENTICACION. Este componente recopila la información del formulario de inicio de sesión. Mantiene el estado del deviceID, email y password. El componente acepta estas propiedades: 
+//onLogin: se ejecuta al hacer click en Iniciar sesión
+//loading: cuando el envío del formulario está en curso
+//error: mensaje al haber error
 import React, { useState } from "react";
 
-export function LoginForm({
-  onLogin,
-  loading,
-  error,
-  footerComponent
-}) {
+export function LoginForm({ onLogin, loading, error, footerComponent }) {
+  //const [deviceId, setDeviceId] = useState(""); CAPTURA
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   function onSubmit(event) {
     event.preventDefault();
-    onLogin({ email, password });
+    onLogin({ email, password }); //Aquí incluye deviceId. CAPTURA
   }
 
   return (
     <form className="card login-form" onSubmit={onSubmit}>
       <h3 className="card-heading">Login</h3>
       {!!error ? <h4 className="card-error">{error}</h4> : null}
+
       <div className="row">
         <label>Email</label>
         <input
@@ -27,6 +29,7 @@ export function LoginForm({
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
+
       <div className="row">
         <label>Password</label>
         <input
@@ -36,11 +39,13 @@ export function LoginForm({
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
+
       <div className="row">
         <button type="submit" className="card-btn" disabled={loading}>
           {loading ? "Logging in..." : "Login"}
         </button>
       </div>
+
       {footerComponent}
     </form>
   );
